d46685a9d33045e6e83680a375a1d76ec8086240
YvonB <yvon.bourbotte@arteliagroup.com>
2023-05-04 11:44:10 +0200

    add initial demo db creation

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "ARTELIA_PROD_PARIS"
+
+    workspaces {
+      name = "snowday"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
